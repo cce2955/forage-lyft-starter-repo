d5c6d869211799d554b1c2653c3f6ed87ed99729
chris <cce29555@gmail.com>
2022-08-24 12:04:55 -0400

    unit test

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
 
-from engine import Engine
-from battery import Battery
-from engine.model.carfactory import carFactory
 
-class Car(ABC):
-    def generator(carfactory):
-        carFactory.create_calliope()
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
--- engine.py
+++
@@ -1,7 +0,0 @@
-class engine:
-    def __init__(self, last_service_mileage:int, currentMileage:int):
-        self.last_service_milage:int
-        self.currentMilage:int
-
-    def needs_Service(self, minMileage:int):
-        return self.current_mileage - self.last_service_mileage > minMileage
